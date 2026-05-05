57d2ae38850fb14873e8f10af84a5fd1286e0856
w5170 <100269397+w5170@users.noreply.github.com>
2026-05-05 11:26:08 +0800

    fix: 替换android.support为androidx注解

--- app/src/main/java/com/github/megatronking/netbare/gateway/Interceptor.java
+++ app/src/main/java/com/github/megatronking/netbare/gateway/Interceptor.java
@@ -1,6 +1,6 @@
 package com.github.megatronking.netbare.gateway;
 
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
